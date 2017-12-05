fa916094e82a0207dfad2ed700beb052507810b6
gabripeti@gmail.com <gabripeti@gmail.com>
2017-12-05 17:02:52 +0100

    Build map from JSON, refactor screen

--- script.js
+++ script.js
@@ -2,24 +2,19 @@ window.onload = function () {
     var marioLeft = 20;
     var backgroundPosition = 0;
 
-    /*function spawnMario() {
-        marioLeft = 20;
-        document.querySelector(".mario").style.top = "184px";
-        document.querySelector(".mario").style.left = marioLeft + "px";
-    }
-
-    spawnMario();*/
-
     $.getJSON("objects.json", function (json) {
-        console.log(json[0]);
-        var newObject = document.createElement("DIV");
-        document.querySelector(".screen").appendChild(newObject);
-        newObject.style.width = json[0].dw + "px";
-        newObject.style.height = json[0].dh + "px";
-        newObject.style.background = "url('objects.png') -" + json[0].sx + "px " + json[0].sy + "px";
-        newObject.style.position = "absolute";
-        newObject.style.top = json[0].dy + "px";
-        newObject.style.left = json[0].dx + "px";
+        var newObject;
+
+        json.forEach(function (jsonObject) {
+            newObject = document.createElement("DIV");
+            document.querySelector(".map").appendChild(newObject);
+            newObject.style.position = "absolute";
+            newObject.style.top = jsonObject.dy + "px";
+            newObject.style.left = jsonObject.dx + "px";
+            newObject.style.width = jsonObject.dw + "px";
+            newObject.style.height = jsonObject.dh + "px";
+            newObject.style.background = "url('objects.png') -" + jsonObject.sx + "px " + jsonObject.sy + "px";
+        });
     });
 
     document.onkeydown = function (event) {
@@ -29,8 +24,8 @@ window.onload = function () {
                 if (marioLeft > 0) {
                     marioLeft -= 2;
                 }
-                if (marioLeft < 20 && backgroundPosition < 0) {
-                    marioLeft = 20;
+                if (marioLeft < 48 && backgroundPosition < 0) {
+                    marioLeft = 48;
                     backgroundPosition += 2;
                 }
                 break;
@@ -39,14 +34,13 @@ window.onload = function () {
                 if (marioLeft < 224) {
                     marioLeft += 2;
                 }
-                if (marioLeft > 204 && backgroundPosition > -3144) {
-                    marioLeft = 204;
+                if (marioLeft > 176 && backgroundPosition > -3144) {
+                    marioLeft = 176;
                     backgroundPosition -= 2;
                 }
                 break;
-
         }
         document.querySelector(".mario").style.left = marioLeft + "px";
-        document.querySelector(".screen").style.backgroundPositionX = backgroundPosition + "px";
+        document.querySelector(".map").style.left = backgroundPosition + "px";
     };
 };
