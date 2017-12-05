c6ea1ed1a3110d3fbb220dbd62e2f2be82e3bae0
gabripeti@gmail.com <gabripeti@gmail.com>
2017-12-05 14:41:41 +0100

    Add background scrolling

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-window.onload = function() {
-    marioLeft = 0;
+window.onload = function () {
+    var marioLeft = 20;
+    var backgroundPosition = 0;
+
+    /*function spawnMario() {
+        marioLeft = 20;
+        document.querySelector(".mario").style.top = "184px";
+        document.querySelector(".mario").style.left = marioLeft + "px";
+    }
+
+    spawnMario();*/
+
     document.onkeydown = function (event) {
-         switch (event.key) {
+        switch (event.key) {
             case "ArrowLeft":
                 console.log("left");
                 if (marioLeft > 0) {
-                    marioLeft -= 1;
+                    marioLeft -= 2;
+                }
+                if (marioLeft < 20 && backgroundPosition < 0) {
+                    marioLeft = 20;
+                    backgroundPosition += 2;
                 }
                 break;
             case "ArrowRight":
                 console.log("right");
                 if (marioLeft < 224) {
-                    marioLeft += 1;
+                    marioLeft += 2;
+                }
+                if (marioLeft > 204 && backgroundPosition > -3144) {
+                    marioLeft = 204;
+                    backgroundPosition -= 2;
                 }
                 break;
+
         }
         document.querySelector(".mario").style.left = marioLeft + "px";
+        document.querySelector(".screen").style.backgroundPositionX = backgroundPosition + "px";
     };
 };
